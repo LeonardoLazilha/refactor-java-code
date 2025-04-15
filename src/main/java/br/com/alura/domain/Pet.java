852dc3c5eb4c54f4de1aa53feb71e69df8dba880
leonardolazilha <lazilhaleonardo@gmail.com>
2025-04-14 21:03:46 -0300

    converter o json em entidade pet

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -2,6 +2,7 @@ package br.com.alura.domain;
 
 public class Pet {
 
+    private Long id;
      private String tipo;
      private String nome;
      private String raca;
@@ -15,6 +16,62 @@ public class Pet {
         this.raca = raca;
         this.idade = idade;
         this.cor = cor;
+        this.peso = peso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Float peso) {
         this.peso = peso;
     }
 }
